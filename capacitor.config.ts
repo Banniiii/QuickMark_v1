b0eed741b814e44e36413f045cdb8f5727b7b78e
Yvan M. <maribbayyvan0@gmail.com>
2025-01-16 07:40:38 +0800

    Initial commit - Angular QuickMark app setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'quickmark_v1',
+  webDir: 'www'
+};
+
+export default config;
