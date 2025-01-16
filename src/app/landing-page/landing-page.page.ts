410fa4e6a0d1d2b0c05f8f6090610fb07ba4c7d5
Yvan M. <maribbayyvan0@gmail.com>
2025-01-16 08:05:38 +0800

    edited landing page

--- src/app/landing-page/landing-page.page.ts
+++ src/app/landing-page/landing-page.page.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-landing-page',
   templateUrl: './landing-page.page.html',
   styleUrls: ['./landing-page.page.scss'],
+  standalone: false,
 })
 export class LandingPagePage implements OnInit {
 
