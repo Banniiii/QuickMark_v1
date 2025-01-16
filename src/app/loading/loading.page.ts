92fc54091458d488c9ea91f9e839ff7e9d9b9afe
Yvan M. <maribbayyvan0@gmail.com>
2025-01-16 08:03:05 +0800

    fix the ts of loading page

--- src/app/loading/loading.page.ts
+++ src/app/loading/loading.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loading',
@@ -7,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class LoadingPage implements OnInit {
+  textToType: string = 'Your friendly QR-coded attendance checker';
+  typedText: string = '';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.typeText(this.textToType, 100);
+    setTimeout(() => {
+      this.router.navigate(['/landing-page']);
+    }, 5000);
+  }
+
+  typeText(text: string, speed: number) {
+    let index = 0;
+    const interval = setInterval(() => {
+      if (index < text.length) {
+        this.typedText += text.charAt(index);
+        document.querySelector('.typing-text')!.textContent = this.typedText;
+        index++;
+      } else {
+        clearInterval(interval); 
+      }
+    }, speed);
   }
 
 }
